db420cd3c991a1e6083df9c6599805a9cc1a09d2
Kazuki Sawada <kazuki@6715.jp>
2020-01-28 09:56:48 +0900

    shutdown-script based relaunching

--- index.js
+++ index.js
@@ -2,5 +2,16 @@
 
 const Compute = require("@google-cloud/compute");
 const compute = new Compute();
+const vm = compute.zone(process.env.ZONE).vm(process.env.VM);
 
-exports.run = async () => console.log(await compute.zone(process.env.ZONE).vm(process.env.VM).start());
+exports.run = async () => {
+	const [{status}] = await vm.getMetadata();
+	if (status != "TERMINATED") {
+		console.log(`status is now ${status}. retrying...`);
+		await new Promise(resolve => setTimeout(resolve, 1000));
+		return exports.run();
+	}
+
+	console.log(`status is now ${status}. relaunching instance...`);
+	return vm.start();
+};
